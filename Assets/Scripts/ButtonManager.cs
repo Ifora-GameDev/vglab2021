91c56ce4938004ac8cd46153776fedbc10b8f87d
Teistt <mary.florian@protonmail.com>
2021-11-07 21:22:31 +0100

    quand on appuie sur les boutons du menu ça fait du son!

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -6,12 +6,33 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    [SerializeField] private AudioSource aSource;
+    [SerializeField] private AudioClip sfxCoin;
+    [SerializeField] private AudioClip sfxBtnPress;
+
+    public void Play()
+    {
+        aSource.PlayOneShot(sfxCoin);
+        StartCoroutine(WaitForSound(2));
+    }
+
+    IEnumerator WaitForSound(int id)
+    {
+        yield return new WaitUntil(() => aSource.isPlaying == false);
+        SceneManager.LoadScene(id);
+        Debug.Log("load scene" + id);
+    }
     public void MainMenu()
     {
-        SceneManager.LoadScene(0);
+        //SceneManager.LoadScene(0);
+        aSource.PlayOneShot(sfxBtnPress);
+        StartCoroutine(WaitForSound(0));
     }
+
     public void Credit()
     {
-        SceneManager.LoadScene(1);
+        aSource.PlayOneShot(sfxBtnPress);
+        StartCoroutine(WaitForSound(1));
+        //SceneManager.LoadScene(1);
     }
 }
